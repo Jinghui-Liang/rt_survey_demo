aba42fdd1c39cd3bb57b8a2bcfbaa22edfb7663b
Jinghui Liang <jinghuil@jinghuil-mac.psy.gla.ac.uk>
2023-11-27 18:53:43 +0000

    align config

--- R/db_conf.R
+++ R/db_conf.R
@@ -1,4 +1,4 @@
-template <- "[database]\ndriver = mysql\nport = %s\ndbname = %s\nusername = %s\npassword = %s"
-conf <- sprintf(template, env_port, sql_dbname, env_usr, env_password)
+template <- "[database]\ndriver = mysql\nhost = %s\nport = %s\ndbname = %s\nusername = %s\npassword = %s"
+conf <- sprintf(template, "db", env_port, sql_dbname, env_usr, env_password)
 
 writeLines(conf, 'server/www/private/conf.ini')
