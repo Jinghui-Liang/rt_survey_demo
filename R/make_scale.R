66a7f1ef5f4e98bb3536b54a0a47781553df7633
Jinghui Liang <jinghuil@jinghuil-mac.psy.gla.ac.uk>
2023-12-11 19:37:08 +0000

    change make_scale.R
    gathering all setup into fullScale.csv

--- R/make_scale.R
+++ R/make_scale.R
@@ -2,7 +2,7 @@ if (!interactive()) {
   dat <- data.frame (read.csv (paste0 ("./scalepool/", quest_name),
     header = FALSE))
   } else {
-  dat <- data.frame (read.csv ('./scalepool/testScale.csv', header = FALSE))
+  dat <- data.frame (read.csv ('../scalepool/testScale.csv', header = FALSE))
 }
 
 names(dat)[1] <- "prompt"
---
+++ R/test_new_nake_scale.R
@@ -0,0 +1,4 @@
+# WHEN THIS IS DONE, COPY TO `make_scale.R` AND DELETE THIS
+
+dat <- read.csv ('../scalepool/fullScale.csv', header = TRUE,
+                 na.strings = "")
